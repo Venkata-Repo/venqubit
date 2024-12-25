8e73d521a036134c1f76dcf3aece69bb6c9e2e79
venqubit <venqubit@users.noreply.github.com>
2024-12-25 13:29:13 -0500

    Deleted all files and folders

--- js/script.js
+++
@@ -1,4 +0,0 @@
-// Example JavaScript for button interaction (Optional)
-document.querySelector("button").addEventListener("click", function() {
-    alert("Welcome to my website! Stay tuned for exciting content.");
-});
