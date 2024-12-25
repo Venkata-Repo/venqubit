b4b4b6ecfc6e2f3ab90866bf2b67ed0046c1e234
Venkata Subbarao <162843220+venqubit@users.noreply.github.com>
2024-12-25 12:57:50 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
-    console.log("VENQUBIT website is ready!");
+// Example JavaScript for button interaction (Optional)
+document.querySelector("button").addEventListener("click", function() {
+    alert("Welcome to my website! Stay tuned for exciting content.");
 });
-
